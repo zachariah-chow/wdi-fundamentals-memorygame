3e9ebd787185dec8208da39b8c88f5524397b08a
Zachariah Chow <zachariahchow@gmail.com>
2020-02-05 22:28:57 +0800

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log("User flipped "+ cardOne);
+cardsInPlay.push(cardOne, cardTwo);
+
+console.log(`User flipped ${cardsInPlay[1]}`);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
