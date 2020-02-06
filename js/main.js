72617958dd5f7b937ad14161f2becc3a57d92cd1
Zachariah Chow <zachariahchow@gmail.com>
2020-02-06 23:56:27 +0800

    updated game logic: cards - array of objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-let cards = ["queen", "queen", "king", "king"];
+// let cards = ["queen", "queen", "king", "king"];
+
+let cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
+
 let cardsInPlay = [];
 
 function getRandomIntInclusive(min, max){
@@ -11,11 +35,11 @@ let winStatus = false;
 
 function checkForMatch() {
     if (cardsInPlay.length >= 2) {
-        if (cardsInPlay[0] === cardsInPlay[1] || cardsInPlay [2]) {
-            alert("You found a match!");
-            winStatus = true;
+        if (cardsInPlay[0].rank === cardsInPlay[1].rank || cardsInPlay[0].rank === cardsInPlay[cards.length - 1].rank) {
+        alert("You found a match!");
+        winStatus = true;
         } else {
-            alert("Sorry, try again.");
+        alert("Sorry, try again.");
         }
     } else {
         alert("Please flip another card.");
@@ -23,15 +47,17 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-    if (winStatus === true) {
+    if (winStatus === true || cards.length === 0) {
         return;
     } else {
-        console.log(`User flipped ${cards[cardId]}!`);
+        console.log(`User flipped ${cards[cardId].rank}!`);
+        console.log(`${cards[cardId].cardImage}`);
         cardsInPlay.push(cards[cardId]);
         cards.splice(cardId, 1);
         checkForMatch();
-        flipCard(getRandomIntInclusive(0,cards.length - 1));
     }
 }
 
 flipCard(0);
+flipCard(2);
+flipCard(1);
